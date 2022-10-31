1ee7d0f71ed908d721f2d038a33987beb9f7bd2f
Jontes-Tech <jonatan@jontes.page>
2022-10-31 13:31:02 +0100

    good code

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,15 @@ import * as express from "express";
 import * as fs from "fs";
 import * as cors from "cors";
 import * as dotenv from 'dotenv'
+import rateLimit from 'express-rate-limit'
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
 
 dotenv.config()
 
@@ -11,6 +20,7 @@ let corsOptions = {
   origin: ["http://127.0.0.1:5173", "https://support.jontes.page"],
 };
 app.use(cors(corsOptions));
+app.use(limiter)
 
 app.get("/users/:email", (req, res) => {
   if (fs.existsSync("users/" + req.params.email + ".json")) {
@@ -20,7 +30,7 @@ app.get("/users/:email", (req, res) => {
     res.json({
       realname: user.name,
       subbeduntil: user.subbedUntil,
-      plan: 'Obegränsad'
+      plan: user.plan
     });
   } else {
     res.status(404).json({ Error: "404 User Not Found!" });
